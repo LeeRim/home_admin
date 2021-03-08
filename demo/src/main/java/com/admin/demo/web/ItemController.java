71cbec0da2d095929c4de542fe5af0dbe52b5306
Leeyr <yul2514@bitek.co.kr>
2021-03-08 17:57:46 +0900

    item checkbox 추가

--- demo/src/main/java/com/admin/demo/web/ItemController.java
+++ demo/src/main/java/com/admin/demo/web/ItemController.java
@@ -1,13 +1,14 @@
 package com.admin.demo.web;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.admin.demo.entity.Item;
+import com.admin.demo.entity.Usage;
+import com.admin.demo.service.ItemService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,8 +18,31 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/item")
 public class ItemController {
+	
+	private final ItemService itemService;
+	
 	@RequestMapping("/list")
 	public String viewItemPage() {
 		return "item/list";
+	}
+	
+	@RequestMapping("/data/usage")
+	@ResponseBody
+	public List<Usage> crudUsage() {
+		List<Usage> usageList = itemService.readUsage();
+		
+		itemService.readItem().forEach(i -> {
+			System.out.println(i.getUsage().getUsageName());
+		});
+
+		return usageList;
+	}
+	
+	@RequestMapping("/data/item")
+	@ResponseBody
+	public List<Item> crudItem() {
+		List<Item> itemList = itemService.readItem();
+
+		return itemList;
 	}
 }
--- demo/src/main/resources/static/js/item/list.js
+++ demo/src/main/resources/static/js/item/list.js
@@ -13,6 +13,34 @@
     	this.div_html="";
     	this.list_html="";
     	this.create = function(title){
+    		// #check-div is check template
+    		let div = $("#check-div").clone(true);
+    		div.find("p").text(title);
+    		div.find("div.checkbox").remove();
+    		this.div_html = div.html();
+    	}
+    	this.setList = function(list){
+    		let input_box = $("#check-div .checkbox").clone(true);
+    		let input_html='';
+    		let row_html='';
+    		$.each(list,function(index, row){
+    			row_html=input_box.clone(true);
+    			row_html.find("input").attr("id",row.key);
+    			row_html.find("label").append(row.label);
+    			row_html.find("label").attr("for",row.key);
+    			input_html+=row_html.prop('outerHTML');
+    		});
+    		this.list_html=$(this.div_html).append(input_html).prop('outerHTML');
+    	}
+    	this.add = function(div_id){
+    		$(div_id).children().eq(0).children().eq(0).append($(this.list_html));
+    	}
+    }
+    
+    /*function Check() {
+    	this.div_html="";
+    	this.list_html="";
+    	this.create = function(title){
     		this.div_html = '<div class="check-list"> \
     			<p>'+title+'</p> \
     		</div>';
@@ -32,16 +60,58 @@
     	this.add = function(div_id){
     		$(div_id).children().eq(0).children().eq(0).append(this.list_html);
     	}
+    }*/
+
+    function addCheckHtml(title,list){
+    	let check = new Check();
+    	check.create(title);
+    	check.setList(list);
+    	check.add("#item-table_wrapper");
     }
     
-    let usage_check = new Check();
-    usage_check.create("사용처 선택");
-    usage_check.setList(["1","2","3"]);
-    usage_check.add("#item-table_wrapper");
     
-    let division_check = new Check();
-    division_check.create("구분 선택");
-    division_check.setList(["소모품","비품"]);
-    division_check.add("#item-table_wrapper");
+    $.ajax({
+		type: 'GET',
+		url: '/item/data/usage'
+	}).done(function(data, textStatus, xhr){
+		let usageList=[];
+		let usageMap={};
+		$.each(data,function(index,row){
+			usageMap={};
+			usageMap.key=row.usageKey;
+			usageMap.label=row.usageName;
+			usageList.push(usageMap);
+		});
+		addCheckHtml("사용처 선택",usageList);
+	    
+	}).fail(function(xhr, textStatus, error){
+//		console.log(xhr.responseText);
+	}).always(function(){
+		let divList=[{key:"div_1",label:"소모품"},{key:"div_2",label:"비품"}];
+		addCheckHtml("구분 선택",divList);
+	});
+    
+    $.ajax({
+		type: 'GET',
+		url: '/item/data/item'
+	}).done(function(data, textStatus, xhr){
+		let tr=null;
+		$.each(data,function(index,row){
+			tr = $("#item-table tbody").append("<tr />");
+			console.log(row);
+			tr.append("<td>"+row.usage.usageName+"</td>");
+			tr.append("<td>"+row.division+"</td>");
+			tr.append("<td>"+row.itemName+"</td>");
+			tr.append("<td>"+row.placeFK+"</td>");
+			tr.append("<td>"+row.placeFK+"</td>");
+			tr.append("<td>"+row.spot+"</td>");
+			tr.append("<td>"+row.unit+"</td>");
+			tr.append("<td>"+row.count+"</td>");
+		});
+	    
+	}).fail(function(xhr, textStatus, error){
+//		console.log(xhr.responseText);
+	});
+    
     
 })(jQuery);
