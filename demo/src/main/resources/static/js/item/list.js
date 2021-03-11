5488ed3537be0c46c0ad2f92b69a1602a211bf09
Leeyr <yul2514@bitek.co.kr>
2021-03-11 14:10:12 +0900

    add checkbox rest event

--- demo/src/main/resources/static/js/item/list.js
+++ demo/src/main/resources/static/js/item/list.js
@@ -19,42 +19,28 @@
     		]
     });
     
-    let datas=new Array();
-    $.ajax({
-    	type: 'GET',
-    	url: '/data/item'
-    }).done(function(data, textStatus, xhr){
-    	let d={};
-    	$.each(data,function(index,row){
-    		
-    		d.usageName=row.usage.usageName;
-    		d.division=(row.division=="somo")? "소모품":"비품";
-    		d.itemName=row.itemName;
-    		d.areaName=row.place.areaName;
-    		d.placeName=row.place.placeName;
-    		d.spot=row.spot;
-    		d.unit=row.unit;
-    		d.count=row.count;
-    		d.action="";
-    		datas.push(d);
-    		d={};
-    	});
-    	
-    	item_table.rows.add(datas).draw();
-
-    }).fail(function(xhr, textStatus, error){
-//		console.log(xhr.responseText);
-    });
+    drawTableData(item_table,null);
     
-    $("div.checkbox").off("click").click(function(){
-		let $this_input = $(this).find("input");
-//		let check_bool = $this.find("input").attr("checked");
-		if ($this_input.is(":checked")){
-			$this_input.prop("checked",false);
-		}else{
-			$this_input.prop("checked",true);
-		}
-		console.log($this_input);
+    $.ajax({
+		type: 'GET',
+		url: '/data/usage'
+	}).done(function(data, textStatus, xhr){
+		let usageList=[];
+		let usageMap={};
+		$.each(data,function(index,row){
+			usageMap={};
+			usageMap.key=row.usageKey;
+			usageMap.label=row.usageName;
+			usageList.push(usageMap);
+		});
+		addCheckHtml("usageKey","사용처 선택",usageList);
+	    
+	}).fail(function(xhr, textStatus, error){
+//		console.log(xhr.responseText);
+	}).always(function(){
+		let divList=[{key:"somo",label:"소모품"},{key:"bi",label:"비품"}];
+		addCheckHtml("division","구분 선택",divList);
+		addCheckEvent("#item-table_wrapper");
 	});
     
     
@@ -62,9 +48,10 @@
     function Check() {
     	this.div_html="";
     	this.list_html="";
-    	this.create = function(title){
+    	this.create = function(id,title){
     		// #check-div is check template
     		let div = $("#check-div").clone(true);
+    		div.find("div.check-list").attr("id",id);
     		div.find("p").text(title);
     		div.find("div.checkbox").remove();
     		this.div_html = div.html();
@@ -111,50 +98,73 @@
     		$(div_id).children().eq(0).children().eq(0).append(this.list_html);
     	}
     }*/
+    
+    function drawTableData(table,param){
+    	console.log(param);
+    	let url='/data/item';
+    	if(param!=null){
+    		url+="?"+param;
+    	}
+    	
+        $.ajax({
+        	type: 'GET',
+        	url: url
+        }).done(function(data, textStatus, xhr){
+        	let datas=new Array();
+        	let d={};
+        	$.each(data,function(index,row){
+        		
+        		d.usageName=row.usage.usageName;
+        		d.division=(row.division=="somo")? "소모품":"비품";
+        		d.itemName=row.itemName;
+        		d.areaName=row.place.areaName;
+        		d.placeName=row.place.placeName;
+        		d.spot=row.spot;
+        		d.unit=row.unit;
+        		d.count=row.count;
+        		d.action="";
+        		datas.push(d);
+        		d={};
+        	});
+        	
+        	table.clear();
+        	table.rows.add(datas).draw();
+        }).fail(function(xhr, textStatus, error){
+//    		console.log(xhr.responseText);
+        });
+    }
 
-    function addCheckHtml(title,list){
+    function addCheckHtml(id,title,list){
     	let check = new Check();
-    	check.create(title);
+    	check.create(id,title);
     	check.setList(list);
     	check.add("#item-table_wrapper");
     }
     
-    function addCheckEvent(){
-    	$("div.checkbox").off("click").click(function(){
-    		let $this_input = $(this).find("input");
-//    		let check_bool = $this.find("input").attr("checked");
-    		if ($this_input.is(":checked")){
-    			$this_input.prop("checked",false);
-    		}else{
-    			$this_input.prop("checked",true);
-    		}
+    function addCheckEvent(wrapper_id){
+    	$(wrapper_id+" input[type='checkbox']").click(function(){
+    		let $this_input = $(this);
     		console.log($this_input);
+    		let key="";
+    		let params={};
+    		$(wrapper_id).find("input[type='checkbox']").each(function(index,row){
+    			key = $(row).parents("div.check-list").attr("id");
+    			if(params[key]==null){
+    				params[key]=[];
+    			}
+    			if($(row).prop("checked")){    				
+    				params[key].push($(row).attr("id"));
+    			}
+    		});
+    		let paramStr="";
+    		for(key in params){
+    			paramStr+=key+"=";
+    			paramStr+=params[key].join(",");
+    			paramStr+="&";
+    		}
+    		drawTableData(item_table,paramStr);
     	});
     }
-    
-    
-    $.ajax({
-		type: 'GET',
-		url: '/data/usage'
-	}).done(function(data, textStatus, xhr){
-		let usageList=[];
-		let usageMap={};
-		$.each(data,function(index,row){
-			usageMap={};
-			usageMap.key=row.usageKey;
-			usageMap.label=row.usageName;
-			usageList.push(usageMap);
-		});
-		addCheckHtml("사용처 선택",usageList);
-	    
-	}).fail(function(xhr, textStatus, error){
-//		console.log(xhr.responseText);
-	}).always(function(){
-		let divList=[{key:"div_1",label:"소모품"},{key:"div_2",label:"비품"}];
-		addCheckHtml("구분 선택",divList);
-//		addCheckEvent();
-	});
-    
     
     
 })(jQuery);
